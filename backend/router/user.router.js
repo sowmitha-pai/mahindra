e3ecf92de99761e457b7c858e313916ccd3ac5f1
sowmitha-pai <msowmithapai2001@gmail.com>
2024-01-16 18:15:01 +0530

    nish-fix

--- backend/router/user.router.js
+++ backend/router/user.router.js
@@ -1,23 +1,19 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const UserController = require("../controller/user.controller");
+const UserController = require('../controller/user.controller');
 
-router.get("/get-data", UserController.getData);
-router.get(
-  "/get-data-center-visualization",
-  UserController.centerVisualization
-);
-router.get(
-  "/get-data-sales-trend-zone-wise",
-  UserController.salesTrendZoneWise
-);
-router.get("/get-data-total-sales", UserController.totalSales);
-router.get("/get-data-funnel", UserController.funnelData);
-router.get("/get-data-funnel-entity", UserController.funnelEntityData);
-router.get(
-  "/get-data-entity-percent-difference",
-  UserController.entityPercentDifference
-);
-router.get("/get-data-drill-down", UserController.drillDown);
+router.get('/get-data', UserController.getData);
+router.get('/get-data-center-visualization', UserController.centerVisualization);
+router.get('/get-data-sales-trend-zone-wise',UserController.salesTrendZoneWise);
+router.get('/get-data-total-sales',UserController.totalSales);
+router.get('/get-data-funnel',UserController.funnelData);
+router.get('/get-data-funnel-entity',UserController.funnelEntityData)
+router.get('/get-data-entity-percent-difference',UserController.entityPercentDifference);
+router.get('/get-data-drill-down',UserController.drillDown);
+router.get('/get-data-product-sale',UserController.productSale);
+router.get('/get-data-mahindra-metrics',UserController.mahindraMetrics);
+router.get('/get-data-mahindra-sales-trend',UserController.mahindraSalesTrend);
+router.get('/get-data-expenditure',UserController.expenditure);
+router.get('/get-data-dealership',UserController.dealership);
 
 module.exports = router;
